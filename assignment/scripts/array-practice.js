585b1b2ef0df21c41dbb9f2490f229dd58a81134
Johnny Keple <Johnny.Keple@gmail.com>
2021-03-12 15:56:34 -0600

    Final 1.0

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -38,10 +38,10 @@ console.log('Last animal is a', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-console.log('Last animal is a', animalArray.length);
+console.log('Last animal is a', animalArray[animalArray.length - 1]);
+
 
 //Double Check this stretch goal
-
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
